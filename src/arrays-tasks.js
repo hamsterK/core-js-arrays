4f106b1ccd75ee2ebc84b7b4f7576a209fc3c29c
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2025-10-17 17:57:03 +0200

    feat: add solution for calculateBalance

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -319,8 +319,9 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  const newBalance = arr.map((calculation) => calculation[0] - calculation[1]);
+  return newBalance.reduce((acc, curr) => acc + curr, 0);
 }
 
 /**
